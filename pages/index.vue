1e2034077968865fec2fdf8cff43284b3c5d64ef
JPetsis <snowblind777@gmail.com>
2020-09-01 00:01:52 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Welcome to Dad Jokes!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Welcome to dad jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
