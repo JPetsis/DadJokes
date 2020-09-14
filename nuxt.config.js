753c0ce21c94a89148e66d46618328a75c1bcd54
JPetsis <snowblind777@gmail.com>
2020-09-14 20:28:29 +0200

    heroku deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,7 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: "static",
-  router: {
-    base: "/DadJokes/"
-  },
+  target: "server",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
